f88ec620168b320b3d92dead66d6ea129a301e6b
Pavan Madduri <pavan4devops@gmail.com>
2020-04-13 13:43:19 -0500

    Update acrcleanup.ps1

--- scripts/acrcleanup.ps1
+++ scripts/acrcleanup.ps1
@@ -1,10 +1,11 @@
 $acrName = 'YourACRName'
 
+#List all images in repo deletes all images except last 5
 $repo = az acr repository list --name $acrName
 $repo | Convertfrom-json | Foreach-Object {
     $imageName = $_
     (az acr repository show-tags -n $acrName --repository $_ | 
-        convertfrom-json |) Select-Object -SkipLast 2 | Foreach-Object {
+        convertfrom-json |) Select-Object -SkipLast 5 | Foreach-Object {
         az acr repository delete -n $acrName --image "$imageName:$_"
         }
 }
